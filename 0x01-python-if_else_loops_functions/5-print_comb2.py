66db72b0d4d46b98186b011612d19d5a06037be8
jubix-pixel <jefenedo4@gmail.com>
2022-09-20 14:06:13 +0000

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i < 10:
-        print("0{:d}".format(i), end='')
-        print("," + " ", end='')
+        print("0{:d}".format + "," + " ", end='')
     else:
-        print("{:d}".format(i), end='')
-        print("," + " ", end='')
+        print("{:d}".format + "," + " ", end='')
