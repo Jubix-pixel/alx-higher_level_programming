b1f5aba464d4aacd8e920867a4c50f163da176f7
jubix-pixel <jefenedo4@gmail.com>
2022-09-21 09:29:01 +0000

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def islower(c):
     """This is a function that checks for lowercase character."""
-    if c.islower('a') == True:
-        print("a is lower")
-    if c.islower('H') == False:
-        print("H is upper")
-    if c.islower('A') == False:
-        print("A is upper")
-    if c.islower('3') == False:
-        print("3 is upper")
-    if c.islower('g') == True:
-        print("g is upper")
+    for c in range(97, 123):
+        if c.islower() == True:
+            print("{:c}.format(c) is lower")
+        elif c.islower() == False:
+            print("{:c}.format(c) is upper")
+        else:
+            print("{:d}.format(c) is upper")
