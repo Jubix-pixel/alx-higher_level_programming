c03d518e68df0ddfcd8c0c1977ab17aededf5b57
jubix-pixel <jefenedo4@gmail.com>
2022-09-21 12:15:23 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
             num = 32
         else:
             num = 0
-        print("{:s}".format(ord(str[i]) - num), end='')
+        print("{:c}".format(ord(str[i]) - num), end='')
     print()
