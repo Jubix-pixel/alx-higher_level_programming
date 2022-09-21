fe3a6ccf72748bc8faba445c9ca76174f7de37e2
jubix-pixel <jefenedo4@gmail.com>
2022-09-21 11:04:56 +0000

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if (ord(char) >= 65 and ord(char) <= 90):
-        print("{:s}.format(str)")
-    else:
-        return(str)
+    for str in range(65, 91):
+        if (ord(str) >= 65 and ord(str) <= 90):
+            print("{:s}.format(str)")
+        else:
+            return(str)
