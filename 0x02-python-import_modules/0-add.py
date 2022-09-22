94fa54d31dfe533fb906908fa950ffd08029845e
jubix-pixel <jefenedo4@gmail.com>
2022-09-22 07:10:57 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from add_0 import def add(a, b):
-    a = 1
-    b = 2
-    print("{:d} + {:d} = add(a, b) {:d}".format(a)(b)(a + b))
-    return (a + b)
+import add_0
+a = 1
+b = 2
+print("{:d} + {:d} = add(a, b) {:d}".format(a)(b)(a + b))
+return (a + b)
