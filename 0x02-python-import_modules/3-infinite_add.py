9ef3c42b208581a6ebab3c37b80d7f15494d0f0b
jubix-pixel <jefenedo4@gmail.com>
2022-09-24 05:44:04 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     i = 0
-    n = len(sys.argv) - 1
-    for x in range(1, sys.argv):
-        i = i + int(sys.argv[x])
+    n = len(sys.argv)
+    for x in range(1, n):
+        i += int(sys.argv[x])
     print("{}".format(i))
