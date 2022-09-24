cbc13ef4d71e7ce0febb6260fc67fe88f27abb31
jubix-pixel <jefenedo4@gmail.com>
2022-09-24 05:53:33 -0700

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    i = 0
+    add = 0
     n = len(sys.argv)
     for x in range(1, n):
-        i += int(sys.argv[x])
-    print("{}".format(i))
+        add += int(sys.argv[x])
+    print(add)
