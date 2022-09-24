0a674d498883080a61113c22b87ee9b39abe858c
jubix-pixel <jefenedo4@gmail.com>
2022-09-24 10:39:32 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    name = dir(hidden_4)
-    for i range(name):
-        if name != "_" and name != "_":
+    for name in dir(hidden_4):
+        if name[0] != "_" and name[1] != "_":
             print(name)
