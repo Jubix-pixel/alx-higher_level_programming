d97b4b0bb66c27295b3a38fa46e6e2df2f7aa42b
jubix-pixel <jefenedo4@gmail.com>
2022-09-24 04:57:48 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{} argument.".format(n))
+    else:
+        n = 0
+        for i in n:
+            print(i)
