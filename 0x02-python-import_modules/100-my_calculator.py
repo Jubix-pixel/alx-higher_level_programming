30b0b4a65359355284bb0f9693cc246419fc5b50
jubix-pixel <jefenedo4@gmail.com>
2022-09-25 11:32:52 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv) - 1
-    if n != 3:
+
+    n_args = len(sys.argv) - 1
+    if n_args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     c_op = sys.argv[2]
-    if c_op != "+" and c_op != "-" and c_op != "*" and c_op != "/":
+    if c_op != '+' and c_op != '-' and c_op != '*' and c_op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
     from calculator_1 import add, sub, mul, div
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    if c_op == "+":
+    if c_op == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
-    if c_op == "-":
+    if c_op == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
-    if c_op == "*":
+    if c_op == '*':
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    if c_op == "/":
+    if c_op == '/':
         print("{} / {} = {}.".format(a, b, div(a, b)))
