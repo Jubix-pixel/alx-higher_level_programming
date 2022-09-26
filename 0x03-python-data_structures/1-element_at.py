be862cbdef64f168af2399f80f4a9bd64d42dd68
jubix-pixel <jefenedo4@gmail.com>
2022-09-26 14:35:22 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return(none)
-    length = len(my_list) - 1
-    if idx > length:
+    length = len(my_list)
+    if idx > length - 1:
         return(none)
     return(my_list[idx])
