2996c6bfde4bc8112effede706467b2e85da7bb6
jubix-pixel <jefenedo4@gmail.com>
2022-09-28 08:42:27 -0700

    task -0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
+    new_matrix = matrix[:]
 
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
