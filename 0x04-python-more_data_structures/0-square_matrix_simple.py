5b06c4f06d75e53d04c9d1cc2ab2a368e760faa9
jubix-pixel <jefenedo4@gmail.com>
2022-09-28 06:27:19 -0700

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    for x in matrix:
+        new_matrix = matrix.append(x ** 2)
+    return(new_matrix)
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+More Data Structures invloving Sets and Dictionaries using Lambda, map, filter and reduce Functions
