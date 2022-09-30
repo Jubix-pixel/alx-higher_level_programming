609f58a6876689554224ee28cda802140ef7679c
jubix-pixel <jefenedo4@gmail.com>
2022-09-30 03:45:31 -0700

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_list[i] = replace
-        else:
-            my_list[i]
-        return (new_list)
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
