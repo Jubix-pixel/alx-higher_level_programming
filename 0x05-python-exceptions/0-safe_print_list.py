f91daa8e586f76daafb3ad0763a90195ec237ba4
jubix-pixel <jefenedo4@gmail.com>
2022-10-10 15:09:47 -0700

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            count += 1
+        except IndexError:
+            return (none)
+    return (count)
