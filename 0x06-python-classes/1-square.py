a5da6875f6aec283c284fa1248b76576d9d51add
jubix-pixel <jefenedo4@gmail.com>
2022-10-15 16:31:24 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1-square.py
+""" Define a Square by size. """
+
+
+class Square:
+    """Represent a Square with Size."""
+    def __init__(self, size):
+        self.__size = size
