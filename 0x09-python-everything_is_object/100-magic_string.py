e76c03bbadad42809e166edd741ed0d3e2b21ec1
jubix-pixel <jefenedo4@gmail.com>
2022-10-18 19:08:15 -0700

    task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    for i in range(n):
-        return(“BestSchool” * i[n])
+     magic_string.n = getattr(magic_string, 'n', 0) + 1
+     return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
